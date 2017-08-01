85ae3b163d29f1b8724c9e8ef41a79c55d50afaa
JosefGuxn <josefguxn@gmail.com>
2017-08-01 15:16:09 +0700

    Added

---
+++ src/components/Main/Novel/AddRelease.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-container fluid>
+    <v-layout row justify-center>
+      <v-flex xs12 md6>
+        
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        seriesId: this.$route.params.seriesId
+      }
+    }
+  }
+</script>
