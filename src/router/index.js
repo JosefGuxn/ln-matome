a4f464f575131b24114e2d18b87a84cd1bf8f7e1
JosefGuxn <josefguxn@gmail.com>
2017-08-01 15:18:59 +0700

    Added new route

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,12 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Main from '@/components/Main/Main'
+import Reader from '@/components/Reader/Reader'
 import Home from '@/components/Main/Home'
 import Novels from '@/components/Main/Novel/Novels'
 import Series from '@/components/Main/Novel/Series'
 import AddSeries from '@/components/Main/Novel/AddSeries'
+import AddRelease from '@/components/Main/Novel/AddRelease'
 import Finder from '@/components/Main/Novel/Finder'
 import SignUp from '@/components/Main/User/SignUp'
 
@@ -14,16 +16,21 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'Main',
       component: Main,
       children: [
         { path: '/', component: Home },
         { path: 'novels', component: Novels },
-        { path: 'series/:id', component: Series },
+        { path: 'series/:seriesId', component: Series },
+        { path: 'series/:seriesId/addrelease', component: AddRelease },
         { path: 'addseries', component: AddSeries },
         { path: 'finder', component: Finder },
         { path: 'signup', component: SignUp }
       ]
+    },
+    {
+      path: '/reader/:chapterId',
+      name: 'Reader',
+      component: Reader
     }
   ],
   mode: 'history'
